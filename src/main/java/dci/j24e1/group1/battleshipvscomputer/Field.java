f73845cb600b406988c2e5fdf4a1af538ab7274f
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-05-02 15:05:38 +0200

    new version

--- src/main/java/dci/j24e1/group1/battleshipvscomputer/Field.java
+++ src/main/java/dci/j24e1/group1/battleshipvscomputer/Field.java
@@ -12,24 +12,28 @@ import java.util.ArrayList;
 
 public class Field {
     @FXML
-    private Label counterLabel;
+    private Label topLabel;
     private Ship[][] ships;
     private Button[][] buttons = new Button[10][10];
     private VBox vbox;
     private ArrayList<Ship> hittedShips = new ArrayList<>();
     private int points = 0;
     private int counter = 100;
+    private boolean isUser;
 //    private int lost = 100;
 
-    public Field(VBox vbox, Ship[][] ships, Label counterLabel) {
+    public Field(VBox vbox, Ship[][] ships, boolean isUser, Label label) {
         this.vbox = vbox;
         this.ships = ships;
-        this.counterLabel = counterLabel;
+        this.isUser = isUser;
+        this.topLabel = label;
         createField();
     }
 
 
+
     private void createField() {
+        vbox.getChildren().clear();
         for (int i = 0; i < 10; i++) {
             HBox hBox = new HBox();
 
@@ -41,46 +45,29 @@ public class Field {
                 button.getProperties().put("y", j);
                 hBox.getChildren().add(button);
                 buttons[i][j] = button;
+                if (isUser) {
+                    button.setDisable(true);
+                    if(isShip(i, j)) {
+                        button.setStyle("-fx-background-color: #8480FE");
+                    }
+                }
                 button.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
-
-                        Button button = (Button) event.getSource();
-                        int x = (int) button.getProperties().get("x");
-                        int y = (int) button.getProperties().get("y");
-                        button.setDisable(true);
-
-                        if (ships[x][y].getId() == 0) {
-                            button.setStyle("-fx-background-color: #81D8D0");
-                        }
-                        if (ships[x][y].getId() > 0) {
-
-                            button.setStyle("-fx-background-color: #826D8C");
-                            points += ships[x][y].getId() / 10;
-                            System.out.println(ships[x][y].getId());
-                            System.out.println("Points " + points);
-
-                            hittedShips.add(ships[x][y]);
-                            ArrayList<Ship> hittedShip = isDead(ships[x][y]);
-                            if (hittedShip != null) {
-                                ArrayList<Ship> waterAroundDeadShip = Ships.getShipsAroundShip(hittedShip, ships);
-                                for (Ship Ship : waterAroundDeadShip) {
-                                    buttons[Ship.getX()][Ship.getY()].setStyle("-fx-background-color: #81D8D0");
-                                    buttons[Ship.getX()][Ship.getY()].setDisable(true);
-                                }
-                            }
-                        }
-
-                        if (event.getSource() == button) {
-                            counter -= 1;
-                            counterLabel.setText("You have   " + counter + " shots left");
-                            if (counter == 0 && points < 50) {
+                        GameAction action = new GameAction();
+                        if (action.userAction(event)) {
+                            if(isEnd(getHittedShips())) {
                                 blockAllButtons();
-                                counterLabel.setText("You looser");
+                                topLabel.setText("You WIN!!!!!!!!!!!");
                             }
-
-                            lost();
+                            return;
                         }
+                        while (action.computerAction()) {
+                            if (isEnd(GameAction.getUserField().getHittedShips())) {
+                                blockAllButtons();
+                                topLabel.setText("You are LOOSER!!! HA-HA");
+                            }
+                        };
                     }
                 });
             }
@@ -88,21 +75,8 @@ public class Field {
         }
     }
 
-    private ArrayList<Ship> isDead(Ship current) {
-        ArrayList<Ship> hittedShip = new ArrayList<>();
-        int length = current.getId() / 10;
-        for (Ship ship : hittedShips) {
-            if (ship.getId() == current.getId()) {
-                hittedShip.add(ship);
-            }
-        }
-        if (hittedShip.size() == length) {
-            return hittedShip;
-        }
-        return null;
-    }
-
     private void blockAllButtons() {
+        buttons = GameAction.getComputerField().getButtons();
         for (Button[] button : buttons) {
             for (Button value : button) {
                 value.setDisable(true);
@@ -110,10 +84,24 @@ public class Field {
         }
     }
 
-    private void lost(){
-        if (counter > 0 && points == 50) {
-            blockAllButtons();
-            counterLabel.setText("You WON!!!");
-        }
+    private boolean isShip(int x, int y) {
+        return ships[x][y].getId() > 0;
     }
+
+    private boolean isEnd(ArrayList<Ship> hitted) {
+        return hitted.size() == 20;
+    }
+
+    public Ship[][] getShips() {
+        return ships;
+    }
+
+    public Button[][] getButtons() {
+        return buttons;
+    }
+
+    public ArrayList<Ship> getHittedShips() {
+        return hittedShips;
+    }
+
 }
